15e93fe3e92770bf41673cb4653dfdab0dc5a533
Anastasia Kemmer <anastasiya_kemmer@mail.ru>
2023-11-30 00:22:27 +0300

    some speed added

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ warnings.filterwarnings("ignore", category=UserWarning)
 # fix random seeds for reproducibility
 SEED = 123
 torch.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = True
 np.random.seed(SEED)
 
 
